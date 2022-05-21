a33d1d2ffa291c061f05a95b7431754ba0d4dd64
Katerina Tuckova <tuckova.mail@gmail.com>
2022-05-21 15:32:47 +0200

    seat number displayed - auto

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from '../JourneyDetail';
+import { SelectedSeat } from '../SelectedSeat';
 
 export const Home = (handleJourneyChange) => {
   const [journey, setJourney] = useState(null)
@@ -9,12 +10,19 @@ export const Home = (handleJourneyChange) => {
     setJourney(data)
   }
 
+  console.log(journey)
+
   return (
     < main >
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       <div >
         {journey !== null
           ? <JourneyDetail journey={journey} />
+          : null}
+      </div>
+      <div >
+        {journey !== null
+          ? <SelectedSeat number={journey.autoSeat} />
           : null}
       </div>
     </main >
