cbb4f6f690f6777aad417f398d4ec4bc2c7c65c7
Katerina Tuckova <tuckova.mail@gmail.com>
2022-05-21 10:28:11 +0200

    Added routing

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+export const App = () => {
+
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/reservation" element={<Reservation />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  )
+};
+
