885b2768e2fd3ac9cd5d51dc8786872ae2354ad6
Katerina Tuckova <tuckova.mail@gmail.com>
2022-05-21 12:01:06 +0200

    Linked API with dates

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -13,17 +13,36 @@ const CityOptions = ({ cities }) => {
   )
 }
 
+const DateOptions = ({ dates }) => {
+
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {dates.map((date) => <option key={date.dateBasic} value={date.dateBasic}>{date.dateCs}</option>)}
+    </>
+  );
+};
+
 export const JourneyPicker = ({ onJourneyChange }) => {
   useEffect(() => {
-    fetch("https://apps.kodim.cz/daweb/leviexpress/api/cities")
-      .then((response) => response.json())
-      .then((data) => setCities(data.results))
+    {
+      fetch("https://apps.kodim.cz/daweb/leviexpress/api/cities")
+        .then((response) => response.json())
+        .then((data) => setCities(data.results))
+    };
+    {
+      fetch("https://apps.kodim.cz/daweb/leviexpress/api/dates")
+        .then((response) => response.json())
+        .then((data) => setDates(data.results))
+    };
+
   }, []);
 
   const [fromCity, setFromCity] = useState("")
   const [toCity, setToCity] = useState("")
   const [date, setDate] = useState("")
   const [cities, setCities] = useState([])
+  const [dates, setDates] = useState([])
 
 
   const handleSubmit = (event) => {
@@ -58,12 +77,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
               <select
                 onChange={(event) => setDate(event.target.value)}
                 value={date}>
-                <option value="">Vyberte</option>
-                <option value="datum01">Datum 01</option>
-                <option value="datum02">Datum 02</option>
-                <option value="datum03">Datum 03</option>
-                <option value="datum04">Datum 04</option>
-                <option value="datum05">Datum 05</option>
+                <DateOptions dates={dates} />
               </select>
             </label>
             <div className="journey-picker__controls">
